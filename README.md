6db1b2b2190e2c0167dbbe9bc1dc39d892904a44
Davey1-2 <david.barta2014gmail.com>
2025-05-18 19:02:02 +0200

    backend done,frontend done

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Tento projekt má založený backend na Express v Node.js a frontend na ReactJS.
-Databáze je MongoDB a pro její správu je použitý Mongoose. Frontend je zatím udělaný jen
-jako jednoduchá šablona pro zkoušku komunikace s backendem. 
+Databáze je MongoDB a pro její správu je použitý Mongoose.
 
 Jak zapnout projekt: do terminálu napište "npm install", tímto se vám stáhnou všechny použité programy,
 dále se přes terminál se dostaňte do složky 
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/books');
 const genreRoutes = require('./routes/genres');
 
+
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/booksdb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB Atlas:', err);
+    });
+
 
 app.use(bodyParser.json());
 
@@ -20,4 +27,3 @@ app.use('/api/books', bookRoutes);
 app.use('/api/genres', genreRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
